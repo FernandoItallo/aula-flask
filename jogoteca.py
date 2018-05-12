6cb4458e99f60d397385054abcfa6167b694be6f
itallo <itallo99@gmail.com>
2018-05-12 17:57:25 -0300

    colocando o bootstrap

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from model.Jogo import Jogo
 
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 lista_jogos = []
 
-jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo1 = Jogo('super mario', 'Ação', 'SNES')
 jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
 
 lista_jogos.append(jogo1)
@@ -30,7 +30,7 @@ def criar():
     console = request. form['console']
     jogo = Jogo(nome, categoria, console)
     lista_jogos.append(jogo)
-    return render_template('lista.html', titulo='Jogo', jogos=lista_jogos)
+    return redirect('/')
 
 
 app.run(debug=True)
